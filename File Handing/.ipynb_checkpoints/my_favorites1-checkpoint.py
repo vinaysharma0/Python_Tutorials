a7a6db8efc68c78fa9b18604b030577a76170d0c
Vinay Sharma <svinay0488@gmail.com>
2020-10-22 10:54:02 +0530

    Notebooks

---
+++ File Handing/.ipynb_checkpoints/my_favorites1-checkpoint.py
@@ -0,0 +1,27 @@
+import csv
+
+title = input('title : ').strip().upper()
+ 
+# titles = {}
+with open('src7/favorites/Favorite TV Shows - Form Responses 1.csv','r') as file:
+    reader = csv.DictReader(file)
+    
+    counter = 0
+    for row in reader:
+    #     title = row['title'].strip().upper()
+
+    #     if title in titles:
+    #         titles[title] += 1
+    #     else:
+    #         titles[title] = 1
+
+        
+        if (row['title'].strip().upper() == title):
+            counter += 1
+
+    print(counter)
+# def f(title):
+#     return titles[title]
+
+# for title in sorted(titles, key= lambda title : titles[title], reverse=True):
+#     print(title, titles[title])
